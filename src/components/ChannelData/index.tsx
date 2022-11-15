da42a52eaf19c1ad21f99e4ee06ea7db0a824c89
Herberth Andrade <andrade.engmecanica@gmail.com>
2022-11-15 17:17:37 -0300

    subindo pra Vercel

---
+++ src/components/ChannelData/index.tsx
@@ -0,0 +1,50 @@
+import React, { useRef, useEffect } from 'react';
+import ChannelMessage, { Mention } from '../ChannelMessage';
+
+import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
+
+const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const div = messagesRef.current;
+
+    if (div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef]);
+
+  return (
+    <Container>
+      <Messages ref={messagesRef}>
+        {Array.from(Array(15).keys()).map((n) => (
+          <ChannelMessage
+            key={n}
+            author="Herberth Andrade"
+            date="15/11/2022"
+            content="Minha primeira mensagem"
+          />
+        ))}
+
+        <ChannelMessage
+          author="Valéria Andrade"
+          date="14/11/2022"
+          content={
+            <>
+              <Mention>@Herberth Andrade</Mention>, me da um oi!
+            </>
+          }
+          hasMention
+          isBot
+        />
+      </Messages>
+
+      <InputWrapper>
+        <Input type="text" placeholder="Conversar em #chat-livre" />
+        <InputIcon />
+      </InputWrapper>
+    </Container>
+  );
+};
+
+export default ChannelData;
