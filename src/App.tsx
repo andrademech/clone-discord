755074f17d6007f76716acbca743b267485d8d55
Herberth Andrade <andrade.engmecanica@gmail.com>
2022-11-15 14:09:15 -0300

    novos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Layout from './components/Layout';
 import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Layout />
+
       <GlobalStyles />
     </>
   );
