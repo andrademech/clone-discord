02ef76715c7bccd6e41e12034faf7c5a9a94f261
Herberth Andrade <andrade.engmecanica@gmail.com>
2022-11-15 11:07:31 -0300

    primeiro commit

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  'prettier/prettier': [
+    'error',
+    {
+      singleQuote: true,
+      parser: 'flow',
+    },
+  ],
+};
